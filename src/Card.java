13cdb9efe9fc14231e60158ae4665e598ce6c5bd
Aldrin Aldino <ekakusmayani91@gmail.com>
2024-12-07 23:17:40 +0800

    Refactor Card class to simplify structure and remove unused functionality

--- src/Card.java
+++ src/Card.java
@@ -1,39 +1,15 @@
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 
-public class Card extends JButton {
-    private ImageIcon frontImage;
-    private ImageIcon backImage;
-    private boolean isFlipped = false;
-    private GameLogic gameLogic;
-
-    public Card(ImageIcon frontImage, GameLogic gameLogic) {
-        this.frontImage = frontImage;
-        this.backImage = AssetLoader.getBackImage();
-        this.gameLogic = gameLogic;
-
-        setIcon(backImage);
-        addActionListener(new CardClickListener());
+class Card {
+    String cardName;
+    ImageIcon cardImageIcon;
+    
+    Card(String cardName, ImageIcon cardImageIcon) {
+        this.cardName = cardName;
+        this.cardImageIcon = cardImageIcon;
     }
 
-    public void flip() {
-        if (isFlipped) {
-            setIcon(backImage);
-        } else {
-            setIcon(frontImage);
-        }
-        isFlipped = !isFlipped;
-    }
-
-    public ImageIcon getFrontImage() {
-        return frontImage;
-    }
-
-    private class CardClickListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            gameLogic.cardClicked(Card.this);
-        }
+    public String toString() {
+        return cardName;
     }
 }
