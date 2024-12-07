600ad623a6353a61c1536a6c8041eeddba1bdf26
Kisul12 <134827926+Kisul12@users.noreply.github.com>
2024-12-07 22:51:15 +0800

    Push Main Frame

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        MatchCard matchCard = new MatchCard();
+        matchCard.frame.setVisible(true);
+    }
+}
