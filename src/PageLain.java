2cc68767fb1c1f0ce5b1a40a17c8da713a4e3b24
DzulhiRaihan <dzulhiraihan@gmail.com>
2024-12-08 20:52:18 +0800

    update

--- src/PageLain.java
+++ src/PageLain.java
@@ -1,5 +1,6 @@
 public class PageLain {
     public static void main(String[] args) {
         System.out.println("Welcome to the other page!");
+        System.out.println("This is a different page from the menu page.");
     }
 }
